794db050d03312081e484acc3564dcc7f45bcc5c
matt0418 <mfeldma7@gmail.com>
2019-03-13 16:03:36 -0400

    Finished 7 tests in Dashboard and Display, moving on to Controls

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,17 @@
 // Test away
+import React from 'react'
+import Dashboard from './Dashboard'
+import renderer from 'react-test-renderer'
+import { render, fireEvent } from 'react-testing-library'
+import 'jest-dom/extend-expect'
+
+describe('<Dashboard />', () => {
+    it('Dashboard renders without crashing', () => {
+        render(<Dashboard />)
+    })
+
+    it('matches snapshot', () => {
+        const tree = renderer.create(<Dashboard></Dashboard>)
+        expect(tree.toJSON()).toMatchSnapshot()
+    })
+})
